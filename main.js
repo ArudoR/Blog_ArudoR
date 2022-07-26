affb95a49680c579362990304aee51d0a6403fcc
Aldo Mejia <aldonear@gmail.com>
2022-07-25 19:45:58 -0500

    Agregue funcionalidad al scroll reveal

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 document.querySelector('.menu__btn').addEventListener('click', () => {
     document.querySelector('.nav__menu').classList.toggle('show');
-})
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news__cards', { delay: 500});
+ScrollReveal().reveal('.cards__banner__one', { delay: 500});
+ScrollReveal().reveal('cards__banner__two', { delay: 500});
